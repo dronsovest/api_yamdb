789ba5451a903acc51b2b98bbfa8266ede5d5c92
Rybakov-Ilay <ilay106@yandex.ru>
2020-10-20 19:13:25 +0300

    "Приложение Users_1.1"

--- users/views.py
+++ users/views.py
@@ -1,12 +1,52 @@
+from django.core.mail import send_mail
 from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from users.models import CustomUser
 from .permissions import IsAdmin
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, EmailSerializer
 
 
 class GetConfirmationCode(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = EmailSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        email = serializer.data.get("email")
+        user, created = CustomUser.objects.get_or_create(
+            username=email, email=email)
+
+        confirmation_code = ???????
+        send_mail(
+            f"Код подтверждения для регистрации в YaMDB",
+            f"{confirmation_code}",
+            'yamdb@yandex.test',
+            [f"{email}"],
+            fail_silently=False,
+        )
+        return Response(
+            {"result": "Код подтверждения отправлен на почту"}, status=200
+        )
+
+    def send_msg(email, name, title, artist, genre, price, comment):
+        subject = f"Обмен {artist}-{title}"
+        body = f"""Предложение на обмен диска от {name} ({email})
+
+        Название: {title}
+        Исполнитель: {artist}
+        Жанр: {genre}
+        Стоимость: {price}
+        Комментарий: {comment}
+
+        """
+        send_mail(
+            subject, body, email, ["admin@rockenrolla.net", ],
+        )
+
     pass
 
 
@@ -17,5 +57,5 @@ class GetToken(APIView):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAdmin,)
+    permission_classes = (IsAdmin, IsAuthenticated,)
     lookup_field = 'username'
