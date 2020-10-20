2fed2aa627aaab5c60f941710a5b16823fc92a70
Ermolaev Andrey <dronsovest@yandex.ru>
2020-10-20 18:59:01 +0300

    was added app api

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include 
+from rest_framework.routers import DefaultRouter
+
+from users.views import GetConfirmationCode, GetToken, UserViewSet
+
+router = DefaultRouter()
+
+router.register('users', UserViewSet, basename='users')
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/auth/email/', GetConfirmationCode.as_view()),
+    path('v1/auth/token/', GetToken.as_view()),
+]
