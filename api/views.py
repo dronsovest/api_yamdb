ba6d8902010f47f4d0101d3a09e241003bd864e7
Ermolaev Andrey <dronsovest@yandex.ru>
2020-10-24 06:49:34 +0300

    permissions

--- api/views.py
+++ api/views.py
@@ -1,33 +1,37 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework.pagination import PageNumberPagination
+from .permissions import IsSuperuserPermission
 from .models import Genre, Catigories, Title
 from .serializers import (
     GenreSerializer,
     CatigoriesSerializer,
     TitleSerializer,
 )
+#from rest_framework import permissions
 
-
-class IsSuperuserPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_superuser
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 5
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     serializer_class = GenreSerializer
     queryset = Genre.objects.all()
-    permissions = (IsSuperuserPermission,)
+    pagination_class = StandardResultsSetPagination
+    permission_classes = (IsSuperuserPermission, )
 
 
 class CategoriesViewSet(viewsets.ModelViewSet):
     serializer_class = CatigoriesSerializer
     queryset = Catigories.objects.all()
-    permissions = (IsSuperuserPermission,)
-
+    pagination_class = StandardResultsSetPagination
+    permission_classes = (IsSuperuserPermission,)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     serializer_class = TitleSerializer
     queryset = Title.objects.all()
-    permissions = (IsSuperuserPermission,)
+    pagination_class = StandardResultsSetPagination
+    permission_classes = (IsSuperuserPermission,)
